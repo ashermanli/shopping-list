3b39a11e117adbdb2b0c07aa627e2b580dced4b3
ashermanli <ashermanli@gmail.com>
2022-02-08 10:54:18 -0700

    added input field

--- src/StoreList.js
+++ src/StoreList.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const StoreList = ({title}) =>{
+
+	const [input,setInput] = useState([])
+
+	const handleInput = (e) =>{
+		console.log(e.target.value)
+		setInput(e.target.value)
+	}
+
 	return(
 	<div className='h-3/4 mb-20'>
       <h1 className="text-blue-800 text-5xl">{title}</h1>
-      <div className="border-2 rounded border-solid border-blue-500 h-full flex m-2">This is the container for gualmart</div>
+      <div className="border-2 rounded border-solid border-blue-500 h-full flex flex-col m-2">This is the container for gualmart
+	  <input className='border-2 rounded border-solid h-5 w-auto mx-5' type='text' value={input} onChange={handleInput}></input>
+	  </div>
     </div>
 	)
 }
